6e34a64c88a94879b470a81d9d674b0e782790a5
AaronQCarney20 <59916292+AaronQCarney20@users.noreply.github.com>
2020-11-14 18:23:34 -0500

    Initial Commit

---
+++ Lab11-1/QuarterlySales/Controllers/SalesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuarterlySales.Models;
+using QuarterlySales.Models.Validation;
+
+namespace QuarterlySales.Controllers
+{
+    public class SalesController : Controller
+    {
+        private SalesContext context { get; set; }
+        public SalesController(SalesContext ctx) => context = ctx;
+        public IActionResult Index() => RedirectToAction("Index", "Home");
+
+        public ViewResult Add()
+        {
+            ViewBag.Employees = context.Employees.OrderBy(e => e.LastName).ThenBy(e => e.FirstName).ToList();
+            return View();
+        }
+        public IActionResult Add(Sales sales)
+        {
+            string message = Validate.CheckSales(context, sales);
+            if (!string.IsNullOrEmpty(message))
+            {
+                ModelState.AddModelError(nameof(Sales.EmployeeId), message);
+            }
+            if (ModelState.IsValid)
+            {
+                context.Sales.Add(sales);
+                context.SaveChanges();
+                TempData["message"] = $"Sales added";
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Employees = context.Employees.OrderBy(e => e.LastName).ThenBy(e => e.FirstName).ToList();
+                return View();
+            }
+        }
+
+
+    }
+}
---
+++ Lab11-1/QuarterlySales/Models/Validation/Validate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuarterlySales.Models.Validation
+{
+    public class Validate
+    {
+        public static string CheckEmployee(SalesContext context, Employee employee)
+        {
+            Employee searchEmployee = context.Employees.FirstOrDefault(e => 
+                e.FirstName.ToLower() == employee.FirstName.ToLower()
+                && e.LastName.ToLower() == employee.LastName.ToLower()
+                && e.DateOfBirth == employee.DateOfBirth
+            );
+            return searchEmployee == null ?
+                string.Empty
+                :$"{searchEmployee.FullName} (DOB: {searchEmployee.DateOfBirth?.ToShortDateString()}) is already in the database";
+        }
+
+        public static string CheckManagerEmployeeMatch(SalesContext context, Employee employee)
+        {
+            Employee manager = context.Employees.Find(employee.ManagerId);
+            if(manager != null
+                && manager.FirstName == employee.FirstName
+                && manager.LastName == employee.LastName
+                && manager.DateOfBirth == employee.DateOfBirth)
+            {
+                return $"Manager and employee can't be the same person.";
+            }
+            return string.Empty;
+
+        }
+        public static string CheckSales(SalesContext context, Sales sale)
+        {
+            Sales sales = context.Sales.FirstOrDefault(
+                s => s.EmployeeId == sale.EmployeeId
+                && s.Year == sale.Year
+                && s.Quarter == sale.Quarter);
+
+            if(sales == null)
+            {
+                return string.Empty;
+            }
+
+            Employee employee = context.Employees.Find(sale.EmployeeId);
+            return $"Sales for {employee.FullName} for {sale.Year} Q{sale.Quarter} are already in the database.";
+        }
+    }
+}
