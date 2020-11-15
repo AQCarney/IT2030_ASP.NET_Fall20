53461b81b4242258bfd28524e8956c1d6e7ef81b
AaronQCarney20 <59916292+AaronQCarney20@users.noreply.github.com>
2020-11-15 13:51:31 -0500

    Fixed Missing Winning Combo

--- Lab10/TicTacToe/Models/TicTacToeBoard.cs
+++ Lab10/TicTacToe/Models/TicTacToeBoard.cs
@@ -45,6 +45,7 @@ namespace TicTacToe.Models
                 case true when IsWinner(2, 5, 8, out winningMark):
                 case true when IsWinner(0, 4, 8, out winningMark):
                 case true when IsWinner(2, 7, 6, out winningMark):
+                case true when IsWinner(2, 4, 6, out winningMark):
                     HasWinner = true;
                     break;
                 default:
